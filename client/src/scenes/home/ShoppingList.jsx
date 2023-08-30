d83f5fa4326ea9c46b20aad27f68fe33e4f3ccc1
Gregory Button <gbutton11@hotmail.com>
2023-08-30 10:59:14 -0500

    Reverting previous change

--- client/src/scenes/home/ShoppingList.jsx
+++ client/src/scenes/home/ShoppingList.jsx
@@ -20,8 +20,7 @@ const ShoppingList = () => {
 
   async function getItems() {
     const items = await fetch(
-      // "http://localhost:1337/api/items?populate=image",
-      "https://react-ecommerce-83n.pages.dev/api/items?populate=image",
+      "http://localhost:1337/api/items?populate=image",
       { method: "GET" }
     );
     const itemsJson = await items.json();
